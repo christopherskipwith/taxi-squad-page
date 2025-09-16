569aeeb1e576fe3ec29412592f96d9f9b6577a45
christopherskipwith <christopherskipwith@gmail.com>
2025-09-16 14:25:56 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1204290981751435264"; // your league ID
+export const leagueID = "1204289693810700288"; // your league ID
 export const leagueName = "EF Taxi Squad Page"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
