0668515d3e1ac0a4a98d5f5aa0124ccc6ff04f52
christopherskipwith <christopherskipwith@gmail.com>
2025-09-16 14:30:06 -0400

    Update tabs.js

--- src/lib/utils/tabs.js
+++ src/lib/utils/tabs.js
@@ -2,86 +2,33 @@ import {leagueID} from '$lib/utils/leagueInfo';
 
 export const tabs = [
     {
-        icon: 'home',
-        label: 'Home',
-        dest: '/',
-        key: 'home',
-    },
-    {
-        icon: 'sports',
-        label: 'Matchups',
-        dest: '/matchups',
-        key: 'matchups',
-    },
-    {
-        icon: 'swap_horiz',
-        label: 'Trades & Waivers',
-        dest: '/transactions',
-        key: 'transactions',
-    },
-    {
-        icon: 'article',
-        label: 'Blog',
-        dest: '/blog',
-        key: 'blog',
+            icon: 'sports_football',
+        label: 'League Website',
+        dest: 'https://extraordinarilyfreshffl.com/',
     },
     {
         icon: 'view_comfy',
-        label: 'League Info',
-        nest: true,
-        key: 'league_info',
-        children: [
-            {
-                icon: 'storage',
-                label: 'Rosters',
-                dest: '/rosters',
-            },
-            {
-                icon: 'groups',
-                label: 'Managers',
-                dest: '/managers',
-            },
-            {
-                icon: 'local_fire_department',
-                label: 'Rivalry',
-                dest: '/rivalry',
-            },
-            {
-                icon: 'leaderboard',
-                label: 'Standings',
-                dest: '/standings',
-            },
-            {
-                icon: 'view_comfy',
-                label: 'Drafts',
-                dest: '/drafts',
-            },
-            {
-                icon: 'emoji_events',
-                label: 'Trophy Room',
-                dest: '/awards',
-            },
-            {
-                icon: 'military_tech',
-                label: 'Records',
-                dest: '/records',
-            },
-            {
-                icon: 'history_edu',
-                label: 'Constitution',
-                dest: '/constitution',
-            },
-            {
-                icon: 'sports_football',
-                label: 'Go to Sleeper',
-                dest: `https://sleeper.app/leagues/${leagueID}`,
-            },
-        ]
+        label: 'Taxi Squad Draft',
+        dest: '/drafts',
+    },
+    {
+        icon: 'swap_horiz',
+        label: 'Taxi Squad Transactions',
+        dest: '/transactions',
+    },
+    {
+        icon: 'storage',
+        label: 'Taxi Squad Rosters',
+        dest: '/rosters',
+    },
+    {
+        icon: 'leaderboard',
+        label: 'Rankings',
+        dest: 'https://extraordinarilyfreshffl.com/taxi-squad-draft/',
     },
     {
         icon: 'lightbulb',
-        label: 'Resources',
-        dest: '/resources',
-        key: 'resources',
+        label: 'Go to Sleeper',
+        dest: `https://sleeper.app/leagues/${leagueID}`,
     },
 ];
